2f0c0bb5b3675ae66ad6e247bb8146cc645b1d12
taro <teroro1226@outlook.jp>
2023-04-25 16:36:28 +0900

    feat: add code message and interactive button

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import discord
+from discord.ui import Button, View
 from discord.ext import commands
 import json
 
+# config.jsonからトークンなどの情報を読み込む
 r = open('config.json')
 load = json.load(r)
 
+# Botの設定を行う
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 bot = commands.Bot(command_prefix=load['prefix'], intents=intents)
 
+# ボタンが押された時にメッセージを返す関数
+async def button_callback(interaction):
+    await interaction.response.send_message("Hi!")
+
+# 起動シーケンス
 @bot.event
 async def on_ready():
-    print("起動しました:{}".format(bot.user))
+    print("{}を起動しました".format(bot.user))
 
+# discord botとの対話ができるかを確認するコマンド
 @bot.command()
-async def test(ctx):
-    await ctx.send("return test")
+async def ping(ctx):
+    await ctx.send("pong")
+
+# Helloのリアクションコマンド
+@bot.command()
+async def hello(ctx):
+    button = Button(label="Click", style=discord.ButtonStyle.green, emoji="🥹")
+    button.callback = button_callback
+
+    view = View()
+    view.add_item(button)
+    await ctx.send("Hi!", view=view)
 
 bot.run(load['token'])
